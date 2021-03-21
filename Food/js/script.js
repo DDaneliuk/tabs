ea0a6995b6ae608cdf32b92a8b46d512922c0ef8
DDenys <d.danelyuk@gmail.com>
2021-03-21 17:42:26 +0200

    Add all script

---
+++ Food/js/script.js
@@ -0,0 +1,41 @@
+// 1. hide all tabs
+// 2. show picked tab
+// 3. add listener on tabs heads
+
+window.addEventListener("DOMContentLoaded", () => {
+  let tabs = document.querySelectorAll(".tabheader__item");
+  let tabsContent = document.querySelectorAll(".tabcontent");
+  let tabsParent = document.querySelector(".tabheader__items");
+
+  console.log(tabs);
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = "block";
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
